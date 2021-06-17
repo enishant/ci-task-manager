07dc4ec1f3cabbd09d7b9e6d51fb4100d0491cfd
Nishant Vaity <enishant@gmail.com>
2021-06-17 12:34:23 +0530

    Adding Model for CRUD operations

---
+++ application/models/Crud.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author  Nishant Vaity
+ * @version v1.0
+ */
+if ( ! defined( 'BASEPATH' ) ) {
+	exit( 'No direct script access allowed' );
+}
+
+class Crud extends CI_Model {
+
+	public function __construct() {
+		parent::__construct();
+	}
+}
