8180bc6cf457c9cbb5f73fff2351d298e11bfeb4
Nishant Vaity <enishant@gmail.com>
2021-06-17 12:35:21 +0530

    CRUD Model: Insert data

--- application/models/Crud.php
+++ application/models/Crud.php
@@ -12,4 +12,27 @@ class Crud extends CI_Model {
 	public function __construct() {
 		parent::__construct();
 	}
+
+	public function insert( $table = '', $data = array() ) {
+		if ( ! empty( $table ) && ! empty( $data ) ) {
+			$this->db->insert( $table, $data );
+			if ( $this->db->insert_id() > 0 ) {
+				return array(
+					'status' => 'success',
+					'message' => 'Data inserted successfully',
+					'id' => $this->db->insert_id(),
+				);
+			} else {
+				return array(
+					'status' => 'error',
+					'message' => 'Unable to insert data',
+				);
+			}
+		} else {
+			return array(
+				'status' => 'error',
+				'message' => 'Incomplete data',
+			);
+		}
+	}
 }
