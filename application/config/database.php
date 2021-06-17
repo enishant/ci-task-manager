0c3b5145193d2d61f932aa01f25b639af2bba560
Nishant Vaity <enishant@gmail.com>
2021-06-17 13:37:06 +0530

    Configured & loaded database and session

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'database', // using hostname provided by docker.
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'taskmanager',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
