1f209ae2b23e0855228883fe6472be068929d146
Nishant Vaity <enishant@gmail.com>
2021-07-05 11:31:15 +0530

    Updated index entry page to handle logged in / non-logged in users

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -24,19 +24,27 @@ class Welcome extends CI_Controller {
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
+	 *      http://example.com/index.php/welcome
+	 *  - or -
+	 *      http://example.com/index.php/welcome/index
+	 *  - or -
 	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
 	 * map to /index.php/welcome/<method_name>
+	 *
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
+	public function index() {
+		$result = logged_in_user_status();
+		if ( $result['status'] == 'success' ) {
+			redirect( base_url( 'dashboard' ) );
+		} else {
+			redirect( base_url( '/login' ) );
+			exit();
+		}
+	}
+
 	}
 }
